070677c91479a9223c0f5be6257d0bf86129f70c
KBeKind <klee@LAPTOP-5P7KDISB>
2023-12-29 13:16:25 -0600

    commit

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,25 @@ fn main() {
         println!("not proceeding");
     }
 
-    let height: i32 = 190;
-    if height > 180 {
-        println!("tall");
-    } else if height > 170 {
-        println!("average");
-    } else {
-        println!("short");
-    }
+    let mut height: i32 = 190;
+    let result: &str = if height > 180 {
+            "Tall"
+        } else if height > 170 {
+            "Average"
+        } else {
+            "Short"
+        };
+
+    // VARIABLE SHADOWING
+    // USED mut TO MAKE VARIABLE MUTABLE
+    height = height - 20;
+
+    println!("The result is: {}", result);
+
+
+    let health: &str = if height < 180 {"good"} else {"unknown"};
+    println!("The health is: {}", health);
+
+    
 
 }
